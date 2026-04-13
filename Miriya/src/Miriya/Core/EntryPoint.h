33b2dbe430d1a21d883253b6d017dfcffb2f4884
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-04-13 19:23:20 +0800

    visible profiler

--- Miriya/src/Miriya/Core/EntryPoint.h
+++ Miriya/src/Miriya/Core/EntryPoint.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "Miriya/Core/Core.h"
 
 #ifdef MIR_PLATFORM_WINDOWS
 
@@ -14,9 +14,17 @@ int main(int argc, char** argv) {
     Miriya::Log::Init();
     MIR_CORE_WARN("Initialized Log");
 
+    MIR_PROFILE_BEGIN_SESSION("Startup", "../../Sandbox/Miriya-Profile-Startup.json");
     auto app = Miriya::CreateApplication();
+    MIR_PROFILE_END_SESSION();
+
+    MIR_PROFILE_BEGIN_SESSION("Runtime", "../../Sandbox/Miriya-Profile-Runtime.json");
     app->Run();
+    MIR_PROFILE_END_SESSION();
+
+    MIR_PROFILE_BEGIN_SESSION("Shutdown", "../../Sandbox/Miriya-Profile-Shutdown.json");
     delete app;
+    MIR_PROFILE_END_SESSION();
 }
 
 #endif
--- Miriya/src/mirpch.h
+++ Miriya/src/mirpch.h
@@ -13,7 +13,7 @@
 #include <unordered_map>
 #include <unordered_set>
 
-
+#include "Miriya/Debug/Instrumentor.h"
 
 #ifndef MIR_PLATFORM_WINDOWS
     #include <Windows.h>
