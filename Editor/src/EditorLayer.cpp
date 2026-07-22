cbfe42347c7b15058e999ba876092748482f7ee8
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-07-22 15:12:50 +0800

    Transform component now stores translation rotation scale & component ui

--- Editor/src/EditorLayer.cpp
+++ Editor/src/EditorLayer.cpp
@@ -47,22 +47,30 @@ void EditorLayer::OnAttach()
     public:
         void OnCreate() override
         {
-            auto& transform = GetComponent<TransformComponent>().Transform;
-            transform[3][0] = rand() % 10 - 5.0f;
+            auto& translation = GetComponent<TransformComponent>().Translation;
+            translation.x     = rand() % 10 - 5.0f;
         }
 
         void OnDestroy() override {}
 
         void OnUpdate(Timestep ts) override
         {
-            auto& transform = GetComponent<TransformComponent>().Transform;
+            auto& translation = GetComponent<TransformComponent>().Translation;
 
             float speed = 5.0f;
 
-            if (Input::IsKeyPressed(Key::A)) transform[3][0] -= speed * ts;
-            if (Input::IsKeyPressed(Key::D)) transform[3][0] += speed * ts;
-            if (Input::IsKeyPressed(Key::W)) transform[3][1] += speed * ts;
-            if (Input::IsKeyPressed(Key::S)) transform[3][1] -= speed * ts;
+            if (Input::IsKeyPressed(Key::A)) {
+                translation.x -= speed * ts;
+            }
+            if (Input::IsKeyPressed(Key::D)) {
+                translation.x += speed * ts;
+            }
+            if (Input::IsKeyPressed(Key::W)) {
+                translation.y += speed * ts;
+            }
+            if (Input::IsKeyPressed(Key::S)) {
+                translation.y -= speed * ts;
+            }
         }
     };
 
--- Miriya/src/Miriya/Scene/Scene.cpp
+++ Miriya/src/Miriya/Scene/Scene.cpp
@@ -40,8 +40,8 @@ void Scene::OnUpdate(Timestep ts)
     }
 
     // Render 2D
-    Camera*    mainCamera      = nullptr;
-    glm::mat4* cameraTransform = nullptr;
+    Camera*   mainCamera = nullptr;
+    glm::mat4 cameraTransform;
     {
         auto view = m_Registry.view<TransformComponent, CameraComponent>();
         for (auto entity : view) {
@@ -49,21 +49,21 @@ void Scene::OnUpdate(Timestep ts)
 
             if (camera.Primary) {
                 mainCamera      = &camera.Camera;
-                cameraTransform = &transform.Transform;
+                cameraTransform = transform.GetTransform();
                 break;
             }
         }
     }
 
     if (mainCamera) {
-        Renderer2D::BeginScene(mainCamera->GetProjection(), *cameraTransform);
+        Renderer2D::BeginScene(mainCamera->GetProjection(), cameraTransform);
 
         auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
         for (auto entity : group) {
             auto [transform, sprite] =
                 group.get<TransformComponent, SpriteRendererComponent>(entity);
 
-            Renderer2D::DrawQuad(transform, sprite.Color);
+            Renderer2D::DrawQuad(transform.GetTransform(), sprite.Color);
         }
 
         Renderer2D::EndScene();
